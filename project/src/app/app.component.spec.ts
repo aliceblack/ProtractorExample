505fe4ca9c2c10fe7103b136484d85a28ad8e4b1
aliceblack <blackfluid44@gmail.com>
2018-05-06 18:19:35 +0200

    Calculator component unit tests

--- project/src/app/app.component.spec.ts
+++ project/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, CalculatorComponent
       ],
     }).compileComponents();
   }));
@@ -24,4 +26,40 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
+});
+
+describe('CalculatorComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent, CalculatorComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should have result', async(() => {
+    const calc = new CalculatorComponent();
+    expect(calc.result).toEqual(0);
+  }));
+  it('must sum numbers', async(() => {
+    const calc = new CalculatorComponent();
+    expect(calc.calcSum(5,2)).toEqual(7);
+  }));
+  it('must display the result', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const calc = fixture.debugElement.componentInstance;
+    calc.result=7;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(document.getElementById('calculatorTotal').textContent).toContain('7');
+  }));
+  it('must calculate the correct result based on the input', async(() => {
+    const fixture = TestBed.createComponent(CalculatorComponent);
+    const calc = fixture.debugElement.componentInstance;
+    (<HTMLInputElement>document.getElementById('input1')).value = '5';
+    (<HTMLInputElement>document.getElementById('input2')).value = '3';
+    document.getElementById('calculatorButton').click();
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(document.getElementById('calculatorTotal').textContent).toContain('8');
+  })); 
 });
